a78bce0efe6330c7589b59cc1d5ddc65d1bd9ae0
rogersillito <rogersillito@gmail.com>
2019-05-21 23:07:48 +0100

    drop Framework version to 4.5. Refactor/whitespace

--- GetFileVersionInfoProperty/Program.cs
+++ GetFileVersionInfoProperty/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -9,60 +9,65 @@ using System.Threading.Tasks;
 
 namespace GetFileVersionInfoProperty
 {
-    class Program
+  class Program
+  {
+    [STAThread]
+    static void Main(string[] args)
     {
-        [STAThread]
-        static void Main(string[] args)
-        {
-            Environment.ExitCode = 0;
-            if (!args.Any() || args.Length < 2 || new[] { "/?", "-h", "--help" }.Contains(args[0].ToLower()))
-            {
-                ShowUsage();
-                return;
-            }
+      Environment.ExitCode = 0;
+      if (!args.Any() || args.Length < 2 || new[] { "/?", "-h", "--help" }.Contains(args[0].ToLower()))
+      {
+        ShowUsage();
+        return;
+      }
 
-            var filePath = args[0];
-            var propertyName = args[1];
-            if (!File.Exists(filePath))
-            {
-                Error($"file not found at path ({filePath})");
-                return;
-            }
+      var filePath = args[0];
+      var propertyName = args[1];
+      if (!File.Exists(filePath))
+      {
+        Error($"file not found at path ({filePath})");
+        return;
+      }
 
-            var fv = FileVersionInfo.GetVersionInfo(filePath);
-            if (new[] { "/l", "-l", "--list" }.Contains(propertyName.ToLower()))
-            {
-                var properties = typeof(FileVersionInfo).GetProperties();
-                foreach (var prop in properties.OrderBy(p => p.Name))
-                {
-                    Console.WriteLine(prop.Name);
-                }
-                return;
-            }
+      var fv = FileVersionInfo.GetVersionInfo(filePath);
+      if (new[] { "/l", "-l", "--list" }.Contains(propertyName.ToLower()))
+      {
+        ListProperties();
+        return;
+      }
 
-            var propertyInfo = typeof(FileVersionInfo).GetProperty(propertyName, BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
-            if (propertyInfo == null)
-            {
-                Error($"file property not found ({propertyName})");
-                return;
-            }
+      var propertyInfo = typeof(FileVersionInfo).GetProperty(propertyName, BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
+      if (propertyInfo == null)
+      {
+        Error($"file property not found ({propertyName})");
+        return;
+      }
 
-            var value = propertyInfo.GetValue(fv);
-            Console.WriteLine(value);
-        }
-
-        private static void ShowUsage()
-        {
-            Console.WriteLine("Usage: FileProp.exe FILEPATH PROPERTY_NAME");
-            Console.WriteLine("       FileProp.exe FILEPATH --list");
-            Console.WriteLine("       FileProp.exe FILEPATH -l");
-            Console.WriteLine("       FileProp.exe FILEPATH /l");
-        }
-
-        private static void Error(string msg)
-        {
-            Console.WriteLine("ERROR: " + msg);
-            Environment.ExitCode = 1;
-        }
+      var value = propertyInfo.GetValue(fv);
+      Console.WriteLine(value);
     }
+
+    private static void ListProperties()
+    {
+      var properties = typeof(FileVersionInfo).GetProperties();
+      foreach (var prop in properties.OrderBy(p => p.Name))
+      {
+        Console.WriteLine(prop.Name);
+      }
+    }
+
+    private static void ShowUsage()
+    {
+      Console.WriteLine("Usage: FileProp.exe FILEPATH PROPERTY_NAME");
+      Console.WriteLine("       FileProp.exe FILEPATH --list");
+      Console.WriteLine("       FileProp.exe FILEPATH -l");
+      Console.WriteLine("       FileProp.exe FILEPATH /l");
+    }
+
+    private static void Error(string msg)
+    {
+      Console.WriteLine("ERROR: " + msg);
+      Environment.ExitCode = 1;
+    }
+  }
 }
